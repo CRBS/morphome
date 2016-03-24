48d8c869236d78bd9d1e2cec378ebd89182eb513
Alex Perez <alexjperez@gmail.com>
2016-03-23 17:14:26 -0700

    Added IMOD preprocessing steps for mitochondria.

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,6 @@
 import pyimod
 
-def nucleus(fnamein, fnameout):
-
+def general_prep(fnamein):
     # If a file is supplied, load the file. If not, proceed with the ImodModel
     # object.
     typein = type(fnamein).__name__
@@ -12,17 +11,31 @@ def nucleus(fnamein, fnameout):
 
     # Remove small contours (i.e. contours having <= 2 points. These are likely
     # erroneous, and are prone to creating spikes in the surface in Amira that
-    # result in NaNs at vertices when computing the Shape Index.
+    # can result in NaNs at vertices. 
     mod.removeSmallContours()
     mod.Objects[0].sortContours()
 
-    # Perform initial smoothing of the surface using IMOD. This is primarily to
-    # eliminate the effects of bad alignment and warping. Done in 3 steps:
-    #    1. Remove pre-existing meshs
-    #    2. Smooth the contours in 3D
-    #    3. Remesh using a low resolution mesh 
+    # Remove pre-existing meshes
     mod = pyimod.ImodCmd(mod, 'imodmesh -e')
+
+    return mod   
+
+def mitochondrion(fnamein, fnameout)
+    mod = general_prep(fnamein)
+    # Perform initial smoothing of the surface using IMOD. Done in 2 steps:
+    #    1. Smooth the contours in 3D
+    #    2. Remesh. Do not use a low resolution mesh, as this tends to contract
+    #       mitochondria.
+    mod = pyimod.ImodCmd(mod, 'smoothsurf -nz 15 -dist 25')
+    mod = pyimod.ImodCmd(mod, 'imodmesh -CTs -P 4')
+    pyimod.ImodExport(mod, fnameout, object = 1)
+
+def nucleus(fnamein, fnameout):
+    mod = general_prep(fnamein)
+    # Perform initial smoothing of the surface using IMOD. This is primarily to
+    # eliminate the effects of bad alignment and warping. Done in 2 steps:
+    #    1. Smooth the contours in 3D
+    #    2. Remesh using a low resolution mesh 
     mod = pyimod.ImodCmd(mod, 'smoothsurf -nz 15 -dist 25')
     mod = pyimod.ImodCmd(mod, 'imodmesh -CT -l')
-
     pyimod.ImodExport(mod, fnameout, object = 1)
